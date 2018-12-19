b7d69cefa7ebf31f8177c1dcd812e99a07da9a6c
Jason McNeill <32476631+jasonmcneill@users.noreply.github.com>
2018-12-19 12:52:12 -0700

    Update scripture.js

--- js/fp/scripture.js
+++ js/fp/scripture.js
@@ -78,6 +78,12 @@ fp.scripture = {
         },
         onOpenEnd: function() {
           window.location.hash = 'scripture=' + key;
+          $('.scripture-expand > a').on('click', function(evt){
+            evt.preventDefault();
+            history.back();
+            console.log(evt.target.attributes['href'].value);
+            location.href = evt.target.attributes['href'].value;
+          });
         },
         onCloseEnd: function() {
           var hashVal = window.location.hash;
