5dfc3b9c9d61b3549bdd9bcbdd2b706835f12cf2
Jason McNeill <vrtjason@gmail.com>
2018-12-22 20:01:45 -0700

    Changes

--- lang/template/new-testament-conversion/logic.js
+++ lang/template/new-testament-conversion/logic.js
@@ -23,68 +23,68 @@
           ${mediaHTML}
         </div>
 
-        <p>
-          <strong>${phrase(2)}</strong>
-        </p>
-
         <ol>
-          <li>${phrase(3)}
+          <li><strong>${phrase(2)}</strong>
             <ol>
-              <li>${phrase(4)} &mdash; ${phrase(5)}</li>
-              <li>${phrase(6)} &mdash; ${phrase(7)}</li>
-              <li>${phrase(8)} &mdash; ${phrase(9)}</li>
-              <li>
-                ${phrase(10)} &mdash; ${phrase(11)}<br>
-                ${phrase(12)} &mdash; ${phrase(13)}
+              <li>${phrase(3)}
+                <ol>
+                  <li>${phrase(4)} &mdash; ${phrase(5)}</li>
+                  <li>${phrase(6)} &mdash; ${phrase(7)}</li>
+                  <li>${phrase(8)} &mdash; ${phrase(9)}</li>
+                  <li>
+                    ${phrase(10)} &mdash; ${phrase(11)}<br>
+                    ${phrase(12)} &mdash; ${phrase(13)}
+                  </li>
+                  <li>${phrase(14)} &mdash; ${phrase(15)}</li>
+                  <li>${phrase(16)} &mdash; ${phrase(17)}</li>
+                </ol>
               </li>
-              <li>${phrase(14)} &mdash; ${phrase(15)}</li>
-              <li>${phrase(16)} &mdash; ${phrase(17)}</li>
-            </ol>
-          </li>
-          <li><strong>${phrase(18)}</strong>
-            <ol>
-              <li>${phrase(19)}</li>
-              <li>${phrase(20)}</li>
-              <li>${phrase(21)}</li>
-              <li>${phrase(22)}</li>
+              <li>${phrase(18)}
+                <ol>
+                  <li>${phrase(19)}</li>
+                  <li>${phrase(20)}</li>
+                  <li>${phrase(21)}</li>
+                  <li>${phrase(22)}</li>
+                </ol>
+              </li>
             </ol>
           </li>
           <li><strong>${phrase(23)}</strong>
             <ol>
               <li>
-                <p><strong><em>${phrase(24)}</em></strong></p>
+                <p><strong>${phrase(24)}</strong></p>
                 ${phrase(25)}
               </li>
               <li>
-                <p><strong><em>${phrase(26)}</em></strong></p>
+                <p><strong>${phrase(26)}</strong></p>
                 ${phrase(27)}
               </li>
               <li>
-                <p><strong><em>${phrase(28)}</em></strong></p>
+                <p><strong>${phrase(28)}</strong></p>
                 ${phrase(29)}
               </li>
               <li>
-                <p><strong><em>${phrase(30)}</em></strong></p>
+                <p><strong>${phrase(30)}</strong></p>
                 ${phrase(31)}
               </li>
               <li>
-                <p><strong><em>${phrase(32)}</em></strong></p>
+                <p><strong>${phrase(32)}</strong></p>
                 ${phrase(33)}
               </li>
               <li>
-                <p><strong><em>${phrase(34)}</em></strong></p>
+                <p><strong>${phrase(34)}</strong></p>
                 ${phrase(35)}
               </li>
               <li>
-                <p><strong><em>${phrase(36)}</em></strong></p>
+                <p><strong>${phrase(36)}</strong></p>
                 ${phrase(37)}
               </li>
               <li>
-                <p><strong><em>${phrase(38)}</em></strong></p>
+                <p><strong>${phrase(38)}</strong></p>
                 ${phrase(39)}
               </li>
               <li>
-                <p><strong><em>${phrase(40)}</em></strong></p>
+                <p><strong>${phrase(40)}</strong></p>
                 ${phrase(41)}
               </li>
             </ol>
