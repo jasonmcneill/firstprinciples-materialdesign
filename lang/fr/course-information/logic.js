1773995a9dbba49be08896d14fed0fe18df39538
Jason McNeill <vrtjason@gmail.com>
2019-01-01 17:12:43 -0700

    Changes

---
+++ lang/fr/course-information/logic.js
@@ -0,0 +1,258 @@
+(function(){
+  const data = $(fp.view.content);
+  const phrase = function(phraseID) {
+    const phraseObj = $(data).find('phrase[id=' + phraseID + ']');
+    const phraseHTML = fp.phrase(phraseObj);
+    return phraseHTML;
+  };
+  const key = data.find('content')[0].attributes['key'].value;
+  const title = phrase(1);
+  let mediaHTML = '';
+  const media = function() {
+    const mediaObj = $(data).find('media');
+    mediaHTML = fp.media(mediaObj);
+  };
+  media();
+  const html = `
+    <div class="row fpmodule fpmodule_${key}">
+      <div class="col xl6 offset-xl3 l8 offset-l2 m8 offset-m2 s12">
+
+        <div class="center hide" id="fpmedia">
+          ${mediaHTML}
+        </div>
+
+        <ul class="collection">
+          <li class="collection-item">
+            ${phrase(2)}
+          </li>
+          <li class="collection-item">
+            ${phrase(3)}
+          </li>
+          <li class="collection-item">
+            ${phrase(4)}
+          </li>
+          <li class="collection-item">
+            ${phrase(5)}
+          </li>
+          <li class="collection-item">
+            ${phrase(6)}
+          </li>
+          <li class="collection-item">
+            ${phrase(7)}
+          </li>
+          <li class="collection-item">
+            ${phrase(8)}
+          </li>
+        </ul>
+
+        <h5>
+          ${phrase(9)}
+        </h5>
+
+        <table class="firstColNoWrap">
+          <tbody>
+            <tr>
+              <td>
+                ${phrase(10)}
+              </td>
+              <td>
+                ${phrase(11)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(12)}
+              </td>
+              <td>
+                ${phrase(13)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(14)}
+              </td>
+              <td>
+                ${phrase(15)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(16)}
+              </td>
+              <td>
+                ${phrase(17)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(18)}
+              </td>
+              <td>
+                ${phrase(19)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(20)}
+              </td>
+              <td>
+                ${phrase(21)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(22)}
+              </td>
+              <td>
+                ${phrase(23)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(24)}
+              </td>
+              <td>
+                ${phrase(25)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(26)}
+              </td>
+              <td>
+                ${phrase(27)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(28)}
+              </td>
+              <td>
+                ${phrase(29)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(30)}
+              </td>
+              <td>
+                ${phrase(31)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(32)}
+              </td>
+              <td>
+                ${phrase(33)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(34)}
+              </td>
+              <td>
+                ${phrase(35)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(36)}
+              </td>
+              <td>
+                ${phrase(37)}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h5>
+          ${phrase(38)}
+        </h5>
+
+        <p>
+          ${phrase(39)}
+        </p>
+
+        <ul>
+          <li>
+            ${phrase(40)} &mdash; ${phrase(41)}
+          </li>
+          <li>
+            ${phrase(42)} &mdash; ${phrase(43)} &mdash; ${phrase(44)}
+          </li>
+          <li>
+            ${phrase(45)} &mdash; ${phrase(46)} &mdash; ${phrase(47)}
+          </li>
+          <li>
+            ${phrase(48)} &mdash; ${phrase(49)} &mdash; ${phrase(50)}
+          </li>
+          <li>
+            ${phrase(51)} &mdash; ${phrase(52)}
+          </li>
+          <li>
+            ${phrase(53)} &mdash; ${phrase(54)}
+          </li>
+        </ul>
+
+        <h5>
+          ${phrase(55)}
+        </h5>
+
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                ${phrase(56)}
+              </td>
+              <td>
+                ${phrase(57)}
+              </td>
+              <td>
+                ${phrase(58)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(59)}
+              </td>
+              <td>
+                ${phrase(60)}
+              </td>
+              <td>
+                ${phrase(61)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(62)}
+              </td>
+              <td>
+                ${phrase(63)}
+              </td>
+              <td>
+                ${phrase(64)}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                ${phrase(65)}
+              </td>
+              <td>
+                ${phrase(66)}
+              </td>
+              <td>
+                ${phrase(67)}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        
+      </div>
+    </div>
+  `;
+  $('title').text(title);
+  $(fp.view.containers.title).html(title);
+  $(fp.view.containers.content).html(html);
+  if (mediaHTML !== '') $('#fpmedia').removeClass('hide');
+})();
---
+++ lang/fr/the-kingdom/logic.js
@@ -0,0 +1,173 @@
+(function(){
+  const data = $(fp.view.content);
+  const phrase = function(phraseID) {
+    const phraseObj = $(data).find('phrase[id=' + phraseID + ']');
+    const phraseHTML = fp.phrase(phraseObj);
+    return phraseHTML;
+  };
+  const key = data.find('content')[0].attributes['key'].value;
+  const title = phrase(1);
+  let mediaHTML = '';
+  const media = function() {
+    const mediaObj = $(data).find('media');
+    mediaHTML = fp.media(mediaObj);
+  };
+  media();
+  const html = `
+    <div class="row fpmodule fpmodule_${key}">
+      <div class="col xl10 offset-xl1 l8 offset-l2 m8 offset-m2 s12">
+
+        <div class="center hide" id="fpmedia">
+          ${mediaHTML}
+        </div>
+
+        <p>
+          ${phrase(2)}
+        </p>
+
+        <p>
+          ${phrase(3)}
+        </p>
+
+        <ol>
+
+          <li>${phrase(4)}
+            <ol>
+              <li>${phrase(5)}
+                <ol>
+                  <li>${phrase(6)}</li>
+                  <li>${phrase(7)}</li>
+                  <li>${phrase(8)}</li>
+                  <li>${phrase(9)}</li>
+                </ol>
+              </li>
+              <li>${phrase(10)}
+                <ol>
+                  <li>${phrase(11)}</li>
+                  <li>${phrase(12)}</li>
+                  <li>${phrase(13)}
+                    <ol>
+                      <li>${phrase(14)}</li>
+                      <li>${phrase(15)}</li>
+                      <li>${phrase(16)}</li>
+                      <li>${phrase(17)}</li>
+                    </ol>
+                  </li>
+                  <li>${phrase(18)}</li>
+                  <li>${phrase(19)}</li>
+                  <li>${phrase(20)}</li>
+                </ol>
+              </li>
+            </ol>
+          </li>
+
+          <li><strong>${phrase(21)}</strong>
+            <ol>
+              <li>${phrase(22)}
+                <ol>
+                  <li>${phrase(23)}</li>
+                </ol>
+              </li>
+              <li>${phrase(24)}
+                <ol>
+                  <li>${phrase(25)}</li>
+                  <li>${phrase(26)}</li>
+                  <li>${phrase(27)}</li>
+                  <li>${phrase(28)}</li>
+                  <li>${phrase(29)}</li>
+                  <li>${phrase(30)}</li>
+                  <li>${phrase(31)}</li>
+                  <li>${phrase(32)}</li>
+                </ol>
+              </li>
+            </ol>
+          </li>
+
+          <li><strong>${phrase(33)}</strong>
+            <ol>
+              <li>${phrase(34)}
+                <ol>
+                  <li>${phrase(35)}</li>
+                </ol>
+              </li>
+              <li>${phrase(36)}
+                <ol>
+                  <li>${phrase(37)}</li>
+                  <li>${phrase(38)}</li>
+                </ol>
+              </li>
+              <li>${phrase(39)}
+                <ol>
+                  <li>${phrase(40)}</li>
+                  <li>${phrase(41)}</li>
+                </ol>
+              </li>
+              <li>${phrase(42)}
+                <ol>
+                  <li>${phrase(43)}</li>
+                </ol>
+              </li>
+              <li>${phrase(44)}
+                <ol>
+                  <li>${phrase(45)}</li>
+                  <li>${phrase(46)}</li>
+                  <li>${phrase(47)}</li>
+                </ol>
+              </li>
+              <li>${phrase(48)}
+                <ol>
+                  <li>${phrase(49)}</li>
+                  <li>${phrase(50)}</li>
+                </ol>
+              </li>
+              <li>${phrase(51)}
+                <ol>
+                  <li>${phrase(52)}</li>
+                </ol>
+              </li>
+              <li>${phrase(53)}
+                <ol>
+                  <li>${phrase(54)}</li>
+                </ol>
+              </li>
+              <li>${phrase(55)}
+                <ol>
+                  <li>${phrase(56)}</li>
+                </ol>
+              </li>
+              <li>${phrase(57)}
+                <ol>
+                  <li>${phrase(58)}</li>
+                </ol>
+              </li>
+              <li>${phrase(59)}
+                <ol>
+                  <li>${phrase(60)}</li>
+                </ol>
+              </li>
+            </ol>
+          </li>
+          <li><strong>${phrase(61)}</strong>
+            <ol>
+              <li>${phrase(62)}</li>
+              <li>${phrase(63)}
+                <ol>
+                  <li>${phrase(64)}</li>
+                  <li>${phrase(65)}</li>
+                  <li>${phrase(66)}</li>
+                  <li>${phrase(67)}</li>
+                </ol>
+              </li>
+              <li>${phrase(68)}</li>
+            </ol>
+          </li>
+        </ol>
+        
+      </div>
+    </div>
+  `;
+  $('title').text(title);
+  $(fp.view.containers.title).html(title);
+  $(fp.view.containers.content).html(html);
+  if (mediaHTML !== '') $('#fpmedia').removeClass('hide');
+})();
