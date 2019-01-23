01eb684550c8035398ea345b5dca1fc0346b5b2d
Jason McNeill <vrtjason@gmail.com>
2019-01-23 15:46:36 -0700

    Changes

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var router = express.Router();
-var parser = require('xml2json');
-var fs = require('fs');
-var util = require('util');
+const express = require('express');
+const router = express.Router();
+const parser = require('xml2json');
+const fs = require('fs');
+const util = require('util');
+const keys = fs.readFileSync(__dirname + '/../../lang/template/keys.json', 'UTF-8', function(json) {
+  return json;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const keys = fs.readFileSync(__dirname + '/../../lang/template/keys.json', 'UTF-8', function(json) {
-  	return json;
-  });
+  
   const globals = fs.readFileSync(__dirname + '/../../lang/template/global/content.xml', 'UTF-8', function(xml) {
   	return xml;
   });
@@ -20,6 +21,13 @@ router.get('/', function(req, res, next) {
     globals: parser.toJson(globals),
     dashboard: parser.toJson(dashboard)
   });
+});
+
+router.get('/api/get/:key', function(req, res, next) {
+  let returnObj = {
+    key: req.params.key
+  };
+  res.send(JSON.stringify(returnObj));
 });
 
 module.exports = router;
