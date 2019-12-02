091f4239bdfe789c74af7a72c9bd81a23deae10f
Jason McNeill <32476631+jasonmcneill@users.noreply.github.com>
2019-12-02 12:57:56 -0700

    Enable selective language-based loading of content

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   "globDirectory": ".",
   "globPatterns": [
+    "**/lang/*/global/content.xml",
     "**/*.{css,js,txt,woff,woff2,png,xml,html,json,pdf}"
   ],
   "globIgnores": ['**/icons/high-res.png', '**/lang/*', '**/splashscreens/*', '**/webfonts/*.eot', '**/webfonts/*.svg', '**/webfonts/*.ttf'],
