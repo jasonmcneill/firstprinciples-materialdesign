e7a510ef1ee532fad7ba053bda576d834c4f1e1c
Jason McNeill <32476631+jasonmcneill@users.noreply.github.com>
2019-12-09 10:55:35 -0700

    Update workbox-config.js

--- lang/pt/workbox-config.js
+++ lang/pt/workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": ".",
   "globPatterns": [
-    "**/*.{css,js,txt,woff,woff2,png,xml,html,json,pdf}"
+    "**/*.{css,js,txt,woff,woff2,png,svg,xml,html,json,pdf}"
   ],
   "globIgnores": ['**/splashscreens/*', '**/webfonts/*.eot', '**/webfonts/*.svg', '**/webfonts/*.ttf'],
   "swDest": "sw.js"
