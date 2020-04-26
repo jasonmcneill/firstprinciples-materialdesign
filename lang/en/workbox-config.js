ac2bdf0cb84c473ae59b846edc55d97a6edaf8a9
jasonmcneill <vrtjason@gmail.com>
2020-04-25 18:35:01 -0700

    Update SW

--- lang/en/workbox-config.js
+++ lang/en/workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": ".",
   "globPatterns": [
-    "**/*.{css,js,txt,woff,woff2,png,svg,xml,html,json,pdf}"
+    "**/*.{css,js,ico,txt,woff,woff2,png,svg,xml,html,json,pdf}"
   ],
   "globIgnores": ['**/splashscreens/*', '**/webfonts/*.eot', '**/webfonts/*.svg', '**/webfonts/*.ttf, **/test.js'],
   "swDest": "sw.js"
